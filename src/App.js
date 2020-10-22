475ef0e6ece0416101488467b5a3b72004df53f4
reactinou <reactinou@gmail.com>
2020-10-22 10:27:36 +0000

    test new

--- src/App.js
+++ src/App.js
@@ -2,25 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      password: '',
+      copyButton: 'Copy'
+    };
+  }
+  handleClick() {
+    this.setState({
+      password: Math.random().toString(36).substr(2, 8),
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Generateur de mot de passe
+            
+          </h1>
+          <h3>{this.state.password} <button className="button" onClick={() => { navigator.clipboard.writeText(this.state.password);
+           this.setState({copyButton:'Copied'});
+           this.setState({copyButton:'Copy'})}}>{this.state.copyButton}</button></h3>
+
+          <button className="button" onClick={() => this.handleClick()}>Generate</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
